df6dea652e757ab50881feb2c01a00c14c156b2f
sfai99 <zer2804@gmail.com>
2025-10-17 01:40:19 +0900

    Add files via upload

---
+++ toursketch/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    let scrolling 
+    let scroll_top //header 고정 시작 값
+    let window_w
+    let mobile_size = 1250
+    let pc_mobile
+
+
+})//$(document).ready
